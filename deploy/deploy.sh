99a8a5b0d933ddd4a877f773974fe0b4c1f2eb87
Abdul Wahab Qureshi <its.inevitable@hotmail.com>
2023-03-04 23:43:43 +0000

    Fix deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# RUN THIS FILE FROM THE ROOT DIRECTORY OF THIS PROJECT ONLY.
+
 # This file will
 # - copy all files to another appropriate location.
 # - migrate database changes.
 # - point nginx to new location.
 
 if [[ -z "$1" ]]; then
-    echo 'You must provide the base directory for application to deploy'
-    exit(1)
+    echo 'You must provide the base directory for application to deploy';
+    exit 1;
 fi
 
 # Create server file.
@@ -22,9 +24,12 @@ toActive=$(cat "$1/inactive.txt")
 
 # Copy to correct place.
 newPath="$1/$toActive"
+
+# Issue, this path is relative to where the script is being executed from. Amend where the script is run from.
 cp -R ../ "$newPath"
 
 # Migrate db changes.
+# Swap this for a docker-compose run.
 php "$newPath/artisan" migrate
 
 # Activate new deployment by symlink.
