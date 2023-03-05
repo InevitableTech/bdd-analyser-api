4103638b8a82e8c7903eb0911911293a786535b3
Abdul Wahab Qureshi <its.inevitable@hotmail.com>
2023-03-05 21:40:18 +0000

    Rollback script, most likely will rely on forward fixing instead.

--- deploy/rollback.sh
+++ deploy/rollback.sh
@@ -4,25 +4,45 @@
 # - rollback database changes.
 # - point nginx to old location.
 
+# exit when any command fails
+set -e
+
 if [[ -z "$1" ]]; then
     echo 'You must provide the base directory for application to deploy'
-    exit(1)
+    exit 1;
 fi
 
 # Get which conceptual server needs to be activated.
 toActive=$(cat "$1/inactive.txt")
 
-# Rollback db changes.
-docker-compose -f "$newPath/docker-compose.yml" -f "$newPath/docker-compose-$ENVIRONMENT.yml" run api php artisan migrate:rollback
+# Copy to correct place.
+oldPath="$1/$toActive"
 
-# Switch to old deployment by symlink.
-rm -rf "$1/current"
-ln -s "$newPath" "$1/current"
-
-# Store the deactivated conceptual server in file for next time.
 inactive='green'
 if [[ $toActive == 'green' ]]; then
     inactive='blue'
 fi
+currentPath="$1/$inactive"
+
+echo "Rolling back to '$toActive', path: '$oldPath'"
+
+# Rollback db changes.
+cd $currentPath
+docker-compose -f "docker-compose.yml" -f "docker-compose-$ENVIRONMENT.yml" run api php artisan migrate:rollback
+
+# Switch to old deployment by symlink.
+rm -rf "$1/current"
+ln -s "$oldPath" "$1/current"
 
 echo $inactive > "$1/inactive.txt"
+
+# Spin down active api if deployed.
+cd $currentPath
+if [[ -f docker-compose.yml ]]; then
+    docker-compose -f "docker-compose.yml" -f "docker-compose-$ENVIRONMENT.yml" down
+fi
+
+cd $oldPath
+docker-compose -f "docker-compose.yml" -f "docker-compose-$ENVIRONMENT.yml" up -d api
+
+echo 'Rollback complete, activated: '$inactive
